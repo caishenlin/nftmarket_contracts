a4d6fb54e81657c577847c1bf1cce1500939ceeb
caishenlin <se@cypherx.tech>
2022-04-27 18:48:24 +0300

    update repo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,11 +23,6 @@ import {
   ALCHEMY_PROJECT_ID
 } from './.secrets.json'
 
-/* task("accounts", "Prints the list of accounts").setAction(async (_, { ethers }) => {
-  const accounts = await ethers.provider.listAccounts();
-  accounts.forEach((account) => console.log(account));
-}); */
-
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.4',
@@ -59,7 +54,6 @@ const config: HardhatUserConfig = {
               : undefined,
           }
         : undefined,
-        //chainId: 1337
     },
     localhost: {
       //accounts: accounts(),
