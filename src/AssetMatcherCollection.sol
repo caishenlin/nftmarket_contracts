171562e06867770702faef55bc9b13a8457df026
caishenlin <se@cypherx.tech>
2022-04-28 22:45:20 +0300

    added also punks standard #6

--- src/AssetMatcherCollection.sol
+++ src/AssetMatcherCollection.sol
@@ -15,7 +15,10 @@ contract AssetMatcherCollection is IAssetMatcher {
     function matchAssets(LibAsset.AssetType memory leftAssetType, LibAsset.AssetType memory rightAssetType) public view override returns (LibAsset.AssetType memory) {
         if (
             (rightAssetType.assetClass == LibAsset.ERC721_ASSET_CLASS) ||
-            (rightAssetType.assetClass == LibAsset.ERC1155_ASSET_CLASS)
+            (rightAssetType.assetClass == LibERC721LazyMint.ERC721_LAZY_ASSET_CLASS) ||
+            (rightAssetType.assetClass == LibAsset.ERC1155_ASSET_CLASS) ||
+            (rightAssetType.assetClass == LibERC1155LazyMint.ERC1155_LAZY_ASSET_CLASS) ||
+            (rightAssetType.assetClass == LibAsset.CRYPTO_PUNKS)
         ) {
           (address leftToken) = abi.decode(leftAssetType.data, (address));
           (address rightToken,) = abi.decode(rightAssetType.data, (address, uint));
