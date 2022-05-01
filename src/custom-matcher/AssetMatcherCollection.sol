292b34bc81595db72c389215a954ec6592fab26c
sorxcode <sorxcode@gmail.com>
2022-05-01 19:36:28 +0100

    refactored contract folder

--- src/custom-matcher/AssetMatcherCollection.sol
+++ src/custom-matcher/AssetMatcherCollection.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.4;
 pragma abicoder v2;
 
-import "./IAssetMatcher.sol";
+import "../interfaces/IAssetMatcher.sol";
 
 /*
  * Custom matcher for collection (assetClass, that need any/all elements from collection)
@@ -15,9 +15,7 @@ contract AssetMatcherCollection is IAssetMatcher {
     function matchAssets(LibAsset.AssetType memory leftAssetType, LibAsset.AssetType memory rightAssetType) public view override returns (LibAsset.AssetType memory) {
         if (
             (rightAssetType.assetClass == LibAsset.ERC721_ASSET_CLASS) ||
-            (rightAssetType.assetClass == LibERC721LazyMint.ERC721_LAZY_ASSET_CLASS) ||
             (rightAssetType.assetClass == LibAsset.ERC1155_ASSET_CLASS) ||
-            (rightAssetType.assetClass == LibERC1155LazyMint.ERC1155_LAZY_ASSET_CLASS) ||
             (rightAssetType.assetClass == LibAsset.CRYPTO_PUNKS)
         ) {
           (address leftToken) = abi.decode(leftAssetType.data, (address));
