a9f5bb67e07dea7248bad42f93bcd5e013b3750c
caishenlin <sorxcode@gmail.com>
2022-05-04 21:51:59 +0100

    updated tests

--- test/Token.test.ts
+++ test/Token.test.ts
@@ -1,13 +1,14 @@
 import { expect } from 'chai';
 
-import { ethers, deployments, getNamedAccounts } from 'hardhat';
+import { ethers } from 'hardhat';
 
 describe('Token contract', function () {
   it('Deployment should assign the total supply of tokens to the owner', async function () {
-    await deployments.fixture(['TestERC20']);
-    const { tokenOwner } = await getNamedAccounts();
-    const Token = await ethers.getContract('TestERC20');
-    const ownerBalance = await Token.balanceOf(tokenOwner);
+    const TestERC20 = await ethers.getContractFactory("TestERC20");
+    const Token = await TestERC20.deploy();
+
+    const [tokenOwner] = await ethers.getSigners();
+    const ownerBalance = await Token.balanceOf(tokenOwner.address);
     const supply = await Token.totalSupply();
     expect(ownerBalance).to.equal(supply);
   });
